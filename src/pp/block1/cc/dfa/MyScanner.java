08cd3d87887518d212eedff46cbe3f9b044da9a0
Bjorn <>
2019-04-26 13:53:04 +0200

    exercise 8

--- src/pp/block1/cc/dfa/MyScanner.java
+++ src/pp/block1/cc/dfa/MyScanner.java
@@ -1,11 +1,44 @@
 package pp.block1.cc.dfa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyScanner implements Scanner {
 
     @Override
     public List<String> scan(State dfa, String text) {
-        return null;
+        List<String> stringList = new ArrayList<>();
+        String currentFound = "";
+        String currentString = "";
+        State currentState = dfa;
+        if (text.length() == 0) {
+            if (dfa.isAccepting()) {
+                stringList.add("");
+            }
+        }
+        for (int i=0; i < text.length(); i++) {
+            char character = text.charAt(i);
+            if (!currentState.hasNext(character)) {
+                if (currentFound.equals("")) {
+                    return stringList;
+                } else {
+                    stringList.add(currentFound);
+                    currentFound = "";
+                    currentString = "";
+                    currentState = dfa;
+                    i--;
+                }
+            } else {
+                currentState = currentState.getNext(character);
+                currentString += character;
+                if (currentState.isAccepting()) {
+                    currentFound = currentString;
+                }
+            }
+        }
+        if (!currentFound.equals("")) {
+            stringList.add(currentFound);
+        }
+        return stringList;
     }
 }
